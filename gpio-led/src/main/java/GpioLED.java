048801649013a3cb9ffd7fd2e77dd28dca5951e4
Darin Pope <darin@planetpope.com>
2016-04-05 19:30:07 -0400

    refactor to blnk

--- gpio-led/src/main/java/GpioLED.java
+++ gpio-led/src/main/java/GpioLED.java
@@ -9,20 +9,21 @@ public class GpioLED {
         try {
             // creating the pin with parameter PinState.HIGH
             // will instantly power up the pin
-            final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "PinLED", PinState.HIGH);
-            System.out.println("light is: ON");
+            final GpioPinDigitalOutput led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "PinLED", PinState.HIGH);
+            led1.blink(500,5000);
+            System.out.println("light is blinking");
 
             // wait 2 seconds
             TimeUnit.SECONDS.sleep(2);
 
             // turn off GPIO 1
-            pin.low();
+            led1.low();
             System.out.println("light is: OFF");
             // wait 1 second
             TimeUnit.SECONDS.sleep(1);
             // turn on GPIO 1 for 1 second and then off
             System.out.println("light is: ON for 1 second");
-            pin.pulse(1000, true);
+            led1.pulse(1000, true);
         } finally {
             // release the GPIO controller resources
             gpio.shutdown();
