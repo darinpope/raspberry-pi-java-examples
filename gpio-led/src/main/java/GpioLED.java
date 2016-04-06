ba34caedb87123acbfe363de1b6f263c47842448
Darin Pope <darin@planetpope.com>
2016-04-05 20:22:23 -0400

    change values

--- gpio-led/src/main/java/GpioLED.java
+++ gpio-led/src/main/java/GpioLED.java
@@ -10,20 +10,19 @@ public class GpioLED {
             // creating the pin with parameter PinState.HIGH
             // will instantly power up the pin
             final GpioPinDigitalOutput led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "PinLED", PinState.HIGH);
-            led1.blink(500,5000);
+            led1.blink(500,10000);
             System.out.println("light is blinking");
-
-            // wait 2 seconds
-            TimeUnit.SECONDS.sleep(2);
 
             // turn off GPIO 1
             led1.low();
             System.out.println("light is: OFF");
+
             // wait 1 second
             TimeUnit.SECONDS.sleep(1);
-            // turn on GPIO 1 for 1 second and then off
+
+            // turn on GPIO 1 for 5 second and then off
             System.out.println("light is: ON for 1 second");
-            led1.pulse(1000, true);
+            led1.pulse(5000, true);
         } finally {
             // release the GPIO controller resources
             gpio.shutdown();
